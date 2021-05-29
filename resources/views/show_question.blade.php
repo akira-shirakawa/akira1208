e1eeea92ba2fcc52cc07e6e8c2cb6413ffd74b57
EC2 Default User <ec2-user@ip-172-31-29-192.ec2.internal>
2021-05-29 04:33:05 +0000

    changed

--- resources/views/show_question.blade.php
+++ resources/views/show_question.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="ja">
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet"> 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
+           
     <title>共通一次試験英単語</title>
       <meta content="無料で共通一次試験対策ができるサイト" name="description">
 </head>
@@ -20,9 +23,7 @@
   <img src="{{Auth::user()->image ?? "https://bulma.io/images/placeholders/128x128.png"}}">
   </div>
         </a>
-             <a class="navbar-item" href="../../homework/{{Auth::id()}}"> 
-              課題
-            </a> 
+           
                 <a class="button is-primary" href="{{ route('logout') }}"
  
                     onclick="event.preventDefault();
@@ -40,15 +41,24 @@
                 </form>     
         </div>
       </nav>    
-    <div class="columns">
+    <div class="columns"> 
         <div class="column">
-            
-        </div>
+        
+        </div>　 
         <div class="column is-half">
-            <article class="panel is-link">
+            @if($message2 == "英語")
+                 <div class="autoplay"> 
+                    <div><img src="https://yellowokapi67.sakura.ne.jp/image/daigakunyuusi.png"></div>
+                    <div><img src="https://yellowokapi67.sakura.ne.jp/image/nannkanneitango.png"></div> 
+                    <div><img src="https://yellowokapi67.sakura.ne.jp/image/eiken3.png"></div>
+                    <div><img src="https://yellowokapi67.sakura.ne.jp/image/eiken2-.png"></div>
+                    <div><img src="https://yellowokapi67.sakura.ne.jp/image/eiken2.png"></div>
+                </div>
+            @endif    
+            <article class="panel is-link"> 
                 <p class="panel-heading">
                   {{$message2}} 
-                </p>
+                </p> 
                 @auth
                 @foreach ($message as $key=>$message) 
              <a href="show/{{$key}}" class="target">
@@ -132,6 +142,25 @@
         .panel{
             text-align:center;
         }
-    </style>
+    </style> 
+<style>
+.autoplay img{
+margin:10px;/*画像の周りに余白*/
+width:95%;/*横幅に制限*/
+border:0.5px solid #eee;/*周囲に薄いボーダー*/}
+</style>
+ <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+ 
+<script type="text/javascript"> 
+ $('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll:1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+// 画像下のドット（ページ送り）を表示
+  dots:true,
+});
+  </script>
 </body>
 </html>
